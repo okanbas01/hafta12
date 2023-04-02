cd0520fbe03f04025bb1bdceb159eb8d720cbf2a
Okan Baş <okan@hub.studio>
2023-04-02 17:20:56 +0300

    feat: app1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { useEffect, useState } from 'react';
+
 function App() {
-  return <p>Merhaba.</p>;
+  const [pencereBoyutu, pencereBoyutuGuncelle] = useState({
+    genislik: window.innerWidth,
+    yukseklik: window.innerHeight,
+  });
+
+  // useEffect( ()=>{ console.count("Useeffect çalıştı") } , [] )
+
+  useEffect(() => {
+    pencereBoyutuGuncelle({ genislik: 100, yukseklik: 200 });
+  }, []);
+
+  return (
+    <div>
+      <p>Pencere genişliği: {pencereBoyutu.genislik}</p>
+      <p>Pencere yüksekliği: {pencereBoyutu.yukseklik}</p>
+    </div>
+  );
 }
 
 export default App;
