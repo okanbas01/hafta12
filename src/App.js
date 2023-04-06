81f333ec7f0b2081d750de5a865388f826f188a3
Okan Baş <okan@hub.studio>
2023-04-06 16:14:51 +0300

    feat: useeffect dep array örnegi yapıldı

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import { useEffect, useState } from 'react';
+
 function App() {
-  return <p>Merhaba.</p>;
+  const [sayac, sayacGuncelle] = useState(0);
+  const [ad, adGuncelle] = useState('Ad Tanımsız');
+
+  useEffect(() => {
+    document.title = sayac;
+  }, [sayac]);
+  function adFonksiyonu() {
+    let zaman = new Date().getSeconds();
+    adGuncelle('Ahmet Yılmaz' + zaman);
+  }
+
+  return (
+    <>
+      <p>{sayac}</p>
+      <button
+        onClick={() => {
+          sayacGuncelle((oncekiDeger) => oncekiDeger + 1);
+        }}
+      >
+        {' '}
+        Arttır
+      </button>
+      <p>Ad:{ad}</p>
+      <button onClick={adFonksiyonu}>Ad Güncelle</button>
+    </>
+  );
 }
 
 export default App;
